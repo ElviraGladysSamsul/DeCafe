eec4049e5aceb8a9639354655bc301f16561db06
ElviraGladysSamsul <mhselvira@gmail.com>
2023-10-27 16:23:42 +0700

    DeCafe 1

---
+++ home.php
@@ -0,0 +1,12 @@
+<div class="col-lg mt-2">
+    <div class="card">
+        <div class="card-header">
+            Home
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis natus deleniti animi, quisquam cum atque qui et! Quos blanditiis et quia, excepturi, non iusto soluta perspiciatis quisquam quae sunt eligendi.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
